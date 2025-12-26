7d3c9593a5443f3ba467f9e7ec6b1b4226fafce4
Kavya-Lokireddy <kavya.lokireddy@gmail.com>
2025-12-26 11:19:43 +0530

    added log-analyzer

---
+++ 01-linux/log-analyzer.sh
@@ -0,0 +1,36 @@
+
+#!/bin/bash
+
+# Analyze log file for errors
+LOG_FILE=${1:-"C:\Users\lokir\fullstack-workshop\01-linux\sample-log.txt"}
+
+if [ ! -f "$LOG_FILE" ]; then
+    echo "Error: Log file not found: $LOG_FILE"
+    exit 1
+fi
+
+echo "====== Log Analysis Report ======="
+echo "File: $LOG_FILE"
+echo ""
+total_lines=$(awk 'END {print NR}' sample-log.txt)
+echo "Total Lines: $total_lines"
+echo "-------------------------------"
+echo "INFO: $(grep -c 'INFO' "$LOG_FILE")"
+echo "ERROR: $(grep -c  'ERROR' "$LOG_FILE")"
+echo "WARNING: $(grep -c  'WARNING' "$LOG_FILE")"
+
+echo ""
+echo "--------------------------------"
+IP_LIST=$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' "$LOG_FILE" | sort | uniq)
+
+echo "Unique IP Addresses Found:"
+for ip in $IP_LIST;
+do 
+	echo " -$ip"
+done
+
+echo "==================================="
+
+
+
+
