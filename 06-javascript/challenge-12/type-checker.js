ccbd37fc369204d7d18d87430441129150fb2fdf
Kavya-Lokireddy <kavya.lokireddy@gmail.com>
2025-12-29 12:14:56 +0530

    added type-checker

---
+++ 06-javascript/challenge-12/type-checker.js
@@ -0,0 +1,51 @@
+const typeOf = (value) =>{
+
+    //Handled null
+    if(value === null){
+        return "null";
+    }
+    //handle NaN
+    if(Number.isNaN(value)){
+        return "nan";
+    }
+
+    //Handle primitives & functions
+    const primitiveType = typeof value;
+    if(primitiveType!=='object'){
+        return primitiveType;
+    }
+
+    //handle built-in objects
+    if(Array.isArray(value)) return "array";
+    if(value instanceof Date) return "date";
+    if(value instanceof Map) return "map";
+    if(value instanceof Set) return "set";
+    if(value instanceof RegExp) return "regexp";
+    if(value instanceof Error) return "error";
+    if(value instanceof Promise) return "promise";
+
+    //default object
+    return "object";
+};
+
+console.log(typeOf(null));
+console.log(typeof(undefined));
+
+console.log(typeOf(42));
+console.log(typeOf(NaN));
+console.log(typeOf('hello'));
+console.log(typeOf(true));
+console.log(typeOf(Symbol()));
+
+console.log(typeOf([]));
+
+console.log(typeOf({}));
+
+console.log(typeOf(()=>{}));
+
+console.log(typeOf(new Date()));
+console.log(typeOf(new Map()));
+console.log(typeOf(new Set()));
+console.log(typeOf(/regex/));
+console.log(typeOf(new Error()));
+console.log(typeOf(Promise.resolve()));
